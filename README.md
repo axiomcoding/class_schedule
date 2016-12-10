51973b398ab6185d1bebdc2a1c18128044f9d847
Michael Tejada <michael@axiomcoding.com>
2016-12-09 21:58:28 -0700

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+# Class Schedule
+Your class schedule is defined in the RSpec file, `class_schedule_spec.rb`. Follow the instructions and RSpec tests to manipulate your class schedule to each test passes! 
---
+++ schedule.rb
@@ -0,0 +1,36 @@
+# define a method, swap_first_and_last_class, that takes in a schedule array as an argument
+# swap the first and last class and return the updated array
+# hint: look up how to swap elements in an array
+
+
+
+
+
+
+# define a method, update_bio, that takes in a schedule array as an argument
+# return the updated array
+
+
+
+
+
+
+# define a method, add_film, that takes in a schedule array as an argument
+# add "Film" after "AP Comp Sci" to your schedule and return the updated array
+
+
+
+
+
+
+# define a method, each, that takes in a schedule array as an argument
+# use iteration to return each class in the array
+
+
+
+
+
+
+# define a method, print_schedule_order, that takes in a schedule array as an argument
+# the method should print the order of your schedule to the screen
+# example: "1. Spanish"
